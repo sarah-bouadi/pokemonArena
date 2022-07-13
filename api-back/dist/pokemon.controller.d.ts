bd3ca7bd2d1965a08434f787b8f5640f3ecece36
Souleman <soulte92@gmail.Com>
2022-07-14 01:25:03 +0200

    Code cleaning

--- api-back/dist/pokemon.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Router, Request, Response } from "express";
-export declare class PokemonController {
-    createPokemon(req: Request, res: Response): Promise<void>;
-    getAllPokemons(req: Request, res: Response): Promise<void>;
-    getPokemon(req: Request, res: Response): Promise<void>;
-    buildRoutes(): Router;
-}
--- api-back/dist/pokemon.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PokemonDocument, PokemonProps } from "./pokemon.model";
-export declare class PokemonService {
-    private static instance?;
-    static getInstance(): PokemonService;
-    private constructor();
-    createPokemon(props: PokemonProps): Promise<PokemonDocument>;
-    getAll(): Promise<PokemonDocument[]>;
-    getById(pokemonId: string): Promise<PokemonDocument | null>;
-}
